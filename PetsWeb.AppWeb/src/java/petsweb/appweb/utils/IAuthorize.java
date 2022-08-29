0e2633c2b0867c3f2d188c21142685acc2782cac
Efrain <mg21001ts@esfe.agape.edu.sv>
2022-08-29 16:14:09 -0600

    nuevos cambios

--- PetsWeb.AppWeb/src/java/petsweb/appweb/utils/IAuthorize.java
+++ PetsWeb.AppWeb/src/java/petsweb/appweb/utils/IAuthorize.java
@@ -4,10 +4,9 @@
  */
 package petsweb.appweb.utils;
 
-/**
- *
- * @author OTONIEL
- */
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+
 public interface IAuthorize {
-    
+    void authorize() throws ServletException, IOException;
 }
